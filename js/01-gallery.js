bf931357aeb12352bbb4064961b73557db7cc084
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-22 00:50:01 +0200

    Додає розмітку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// {/* <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div> */}
+console.log(galleryItems)
+const galleryListRef = document.querySelector(".gallery");
+// const galleryMarkup = galleryItems(items);
+
+const createGalleryMarkup = (preview) => {
+    galleryItems.map(item =>{
+        return 
+       `<div class="gallery__item">
+       <a class="gallery__link" href="large-image.jpg">
+         <img
+           class="gallery__image"
+           src="small-image.jpg"
+           data-source="large-image.jpg"
+           alt="Image description"
+         />
+       </a>
+     </div>`
+    })
+    console.log(galleryMarkup)
+}
+
+
+// const markup = galleryItems
+//   .map((galleryItem) => `<li class="list-item"><img class="image" src=${galleryItem.preview}, alt=${galleryItem.description}></li>`)
+//   .join("");
+
+//   galleryListRef.insertAdjacentHTML("beforeend", markup);
+
+// console.log(galleryItems);
