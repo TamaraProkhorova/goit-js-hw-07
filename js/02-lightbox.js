3ea0224904854d815fdff24657a42c657d2d1ffc
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-22 22:33:57 +0200

    Виконує 2ге завдання, підключає бібліотеку SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListRef = document.querySelector(".gallery");
+
+function createImagesCardsMarkup(images) {
+  return images
+  .map
+  ((image) =>
+      `<a class="gallery__item" href="${image.original}">
+       <img
+         class="gallery__image"
+         src="${image.preview}"
+         alt="${image.description}"
+       />
+     </a>`
+  )
+  .join(" ")
+}
+
+const galaryMarkup = createImagesCardsMarkup(galleryItems)
+galleryListRef.insertAdjacentHTML("beforeend", galaryMarkup);
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+})
+
+console.log(galleryListRef)
